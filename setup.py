35eae9b47241aaa5f3ca48417c04c04d9adb455c
gangl <michael.e.gangl@jpl.nasa.gov>
2017-11-01 14:03:23 -0700

    first commit - shale 0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='shale',
+      version='0.1',
+      description='Shale token substitution utility',
+      url='',
+      author='Mike Gangl',
+      author_email='',
+      license='MIT',
+      packages=['shale'],
+      scripts=['bin/shale'],
+      zip_safe=False)
---
+++ shale/shale.py
@@ -0,0 +1,72 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader, Template
+import argparse
+import sys
+import os.path
+import re
+
+
+def usage():
+    print "shale.py < --stage stage> < --config config.yml> <--file file1> [--file file2] ... [--file fileN]"
+
+def env_override(value, key):
+  return os.getenv(key, value)
+
+def run():
+
+
+    parser = argparse.ArgumentParser(description='Config variables subsitatution utility')
+    parser.add_argument('--stage', type=str, required=True,
+                    help='the stage [dev,test,prod] from which to find substitution tokens')
+
+    parser.add_argument('--config', type=str, required=True, metavar='config.yml',
+                        help='configuration yaml file from which to read subtitution tokens')
+
+    parser.add_argument('--file', action='append', metavar='file.cfg.tpl [--file file2.cfg.tpl] ...', required=True,
+                        help='a file on which to do token substitution. Must be of type .tpl')
+
+    args = parser.parse_args()
+
+    ENV = Environment(loader=FileSystemLoader('./'))
+    ENV.filters['env_override'] = env_override
+
+    config = args.config
+
+
+
+
+    if not os.path.isfile(config):
+        print "Config file \"" + config + "\" does not exist. Exiting..."
+        sys.exit(-1)
+
+    with open(config) as file:
+        dictionary =  yaml.load(file)
+
+    if args.stage not in dictionary:
+        print "Stage \"" + args.stage + "\" does not exist in " + config
+        sys.exit(-1)
+
+    vals = dictionary['default']
+    vals.update(dictionary[args.stage])
+
+    for templateFile in args.file:
+        print "Processing templateFile " + templateFile
+
+        if not os.path.isfile(templateFile):
+            print "File \"" + templateFile + "\" does not exist. Skipping..."
+            continue
+
+
+        template = ENV.get_template(templateFile)
+        output = template.render(vals)
+
+        outputFile = re.sub('\.tpl$', '', templateFile)
+
+        # to save the results
+        with open(outputFile, "wb") as fh:
+            fh.write(output)
+
+
+
+if __name__ == "__main__":
+    main()
